e3da7592fe72ed04a7eb7fd4be16a8404e4d4f87
mahfuzurmunna <mahfuzurrm07@gmail.com>
2023-06-07 17:16:46 +0600

    Revert "firebase environment setup done"
    This reverts commit 340fff55ff18841bf18e54d0adcd5c2c20198ec0.

--- src/Components/Routes/Routes.jsx
+++ src/Components/Routes/Routes.jsx
@@ -1,31 +1,12 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
-import { createBrowserRouter } from "react-router-dom";
-import Main from "../Main/Main";
-import Home from "../Pages/Home/Home";
-import Login from "../Pages/Login/Login";
-import Register from "../Pages/Register/Register";
+import React from 'react';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Main />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path:'/login',
-        element: <Login/>
-      },
-      {
-        path: 'register',
-        element: <Register/>
-      },
+const Routes = () => {
+  return (
+    <div>
       
-    ],
-  },
-]);
+    </div>
+  );
+};
 
-export default router;
+export default Routes;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import router from "./Components/Routes/Routes";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
