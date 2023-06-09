bf52456a37ef256a01e72e5fb55646af37b53b89
mahfuzurmunna <mahfuzurrm07@gmail.com>
2023-06-09 19:27:36 +0600

    add toy page design done

--- src/Components/Pages/Add a Toy/Addatoy.jsx
+++ src/Components/Pages/Add a Toy/Addatoy.jsx
@@ -1,10 +1,170 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+
+import add from "../../../assets/add.png";
 
 const Addatoy = () => {
   return (
-    <div>
-      
+    <div className="my-container rounded-xl mb-12 ">
+      <div className="flex flex-col lg:flex-row justify-between items-center gap-12 ">
+        <div className="border-2 border-accent w-full lg:w-[90%] py-12 lg:pt-4 lg:pb-8 px-6 lg:px-12 rounded-3xl">
+          <h2 className="text-4xl rehn-bold my-6">Add Your Toys</h2>
+          <p className="mb-4">
+            Bring forth your cherished playthings, for they shall be added to
+            the realm of my delightful amusements!
+          </p>
+          <form action="">
+            {/* toy name and seller */}
+            <div className="flex gap-6">
+              <div className="w-[50%]">
+                <label htmlFor="">Toy Name</label> <br />
+                <input
+                  type="text"
+                  name="toyname"
+                  placeholder="Enter toy name"
+                  className=" mt-2 mb-4 border-2 border-x-accent-contentfocus w-full px-4 py-3 rounded-3xl outline-accent2"
+                  required
+                />
+              </div>
+              <div className="w-1/2">
+                <label htmlFor="">Seller Name</label> <br />
+                <input
+                  type="text"
+                  name="sellername"
+                  placeholder="Enter name"
+                  className=" mt-2 mb-4 border-2 border-x-accent-contentfocus w-full px-4 py-3 rounded-3xl  outline-accent2"
+                  required
+                />
+              </div>
+            </div>
+
+            {/* seller email and sub-category */}
+            <div className="flex gap-6">
+              <div className="w-[50%]">
+                <label htmlFor="">Seller Email</label> <br />
+                <input
+                  type="email"
+                  name="selleremail"
+                  placeholder="Enter email"
+                  className=" mt-2 mb-4 border-2 border-x-accent-contentfocus w-full px-4 py-3 rounded-3xl  outline-accent2"
+                  required
+                />
+              </div>
+              <div className="w-1/2">
+                <label htmlFor="">Category</label> <br />
+                <select
+                  type="text"
+                  name="category"
+                  // onChange={handleCategory}
+
+                  className=" mt-2 mb-4 border-2 border-x-accent-contentfocus w-full px-4 py-3 rounded-3xl  outline-accent2"
+                  required
+                >
+                  <option className="text-sm text-gray-400" disabled selected>
+                    Select Category
+                  </option>
+
+                  <option value="1" className="text-lg">
+                    Airplane
+                  </option>
+                  <option value="2" className="text-lg">
+                    Car
+                  </option>
+                  <option value="3" className="text-lg">
+                    Puzzle
+                  </option>
+                </select>
+              </div>
+            </div>
+
+            {/* Price and Rating */}
+            <div className="flex gap-6">
+              <div className="w-[50%]">
+                <label htmlFor="">Price</label> <br />
+                <input
+                  type="text"
+                  name="price"
+                  placeholder="Enter price"
+                  className=" mt-2 mb-4 border-2 border-x-accent-contentfocus w-full px-4 py-3 rounded-3xl  outline-accent2"
+                  required
+                />
+              </div>
+              <div className="w-1/2">
+                <label htmlFor="">Rating</label> <br />
+                <select
+                  type="text"
+                  name="rating"
+                  // onChange={handleCategory}
+                  placeholder="Enter Rating"
+                  className=" mt-2 mb-4 border-2 border-x-accent-contentfocus w-full px-4 py-3 rounded-3xl  outline-accent2"
+                  required
+                >
+                  <option className="text-sm" disabled>
+                    ratings
+                  </option>
+                  <option value="1" className="text-lg">
+                    1 Star
+                  </option>
+                  <option value="2" className="text-lg">
+                    2 Star
+                  </option>
+                  <option value="3" className="text-lg">
+                    3 Star
+                  </option>
+                  <option value="4" className="text-lg">
+                    4 Star
+                  </option>
+                  <option value="5" className="text-lg">
+                    5 Star
+                  </option>
+                </select>
+              </div>
+            </div>
+
+            {/* url and quantity */}
+            <div className="flex gap-6">
+              <div className="w-[50%]">
+                <label htmlFor="">Quantity</label> <br />
+                <input
+                  type="number"
+                  name="quantity"
+                  placeholder="Enter quantity"
+                  className=" mt-2 mb-4 border-2 border-x-accent-contentfocus w-full px-4 py-3 rounded-3xl  outline-accent2"
+                  required
+                />
+              </div>
+              <div className="w-1/2">
+                <label htmlFor="">Photo Url</label> <br />
+                <input
+                  type="text"
+                  name="photourl"
+                  placeholder="Enter photo url"
+                  className=" mt-2 mb-4 border-2 border-x-accent-contentfocus w-full px-4 py-3 rounded-3xl  outline-accent2"
+                />
+              </div>
+            </div>
+            {/* detail description */}
+            <label htmlFor="">Description</label>
+            <textarea
+              name="description"
+              rows="5"
+              placeholder="Enter Details"
+              className=" mt-2 mb-4 border-2 border-x-accent-contentfocus w-full px-4 py-3 rounded-3xl  outline-accent2"
+            ></textarea>
+            <input
+              type="submit"
+              className=" transition duration-200  shadow-md  md:mb-0  px-4 py-2 md:px-12 md:py-3 m-2 text-xl rounded-3xl border-transparent border-2 text-white  bg-accent mb-8 w-full cursor-pointer"
+              value="Submit"
+            />
+          </form>
+        </div>
+        <div className="">
+          <img
+            src={add}
+            className="w-[65%] mx-auto lg:w-full rounded-3xl"
+            alt=""
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- src/Components/Pages/Blogs/Blogs.jsx
+++ src/Components/Pages/Blogs/Blogs.jsx
@@ -8,13 +8,124 @@ const Blogs = () => {
   const path = location.pathname;
   return (
     <div className="my-container">
-      <div className='bg-bg text-center p-24'>
+      <div className="bg-bg text-center p-24 rounded-3xl mb-16">
         <small>{path}</small>
-        <h2 className='text-4xl rehn-bold '>
-         Questions & Answers
-        </h2>
+        <h2 className="text-4xl rehn-bold ">Questions & Answers</h2>
       </div>
-      
+      <div className="collapse collapse-plus border-b border-primary rounded w-4/5 mx-auto">
+        <input type="radio" name="my-accordion-3" checked="checked" />
+        <div className="collapse-title text-2xl rehn-bold text-accent ">
+          What is an access token and refresh token? <br /> How do they work and
+          where should we store them on the client-side?
+        </div>
+        <div className="collapse-content w-5/6 text-justify">
+          <p className="">
+            -An access token is a credential issued by an authentication server
+            after user login or permission grant. <br /> <br /> -A refresh token
+            is provided together with the access token and is used to obtain a
+            new access token after the current one expires.
+          </p>
+          <br />
+          <p>
+            <span className="rehn-bold text-accent2">How they work:</span>{" "}
+            <br /> When a user logs in or authenticates, an access token and a
+            refresh token are issued by the authentication server. The access
+            token is sent with each API request to authenticate and authorize
+            access. If the access token expires, the refresh token is used to
+            request a new access token without requiring the user to provide
+            their credentials again. The refresh token is exchanged for a new
+            access token, and the process continues.
+          </p>
+          <br />
+          <p>
+            <span className="rehn-bold text-accent2">How its Stored:</span>{" "}
+            <br />
+            Cookies: Storing tokens as HTTP-only cookies provides some
+            protection against cross-site scripting (XSS) attacks. <br /> <br />{" "}
+            Local storage: Tokens can be stored in the browsers local storage or
+            session storage. However, this method may be vulnerable to XSS
+            attacks. <br /> <br /> Secure client storage: Some frameworks or
+            libraries offer secure storage options designed specifically for
+            storing sensitive information, such as encrypted client-side storage
+            or secure storage modules.
+          </p>
+        </div>
+      </div>
+      {/* second answe */}
+      <div className="collapse collapse-plus border-b border-accent rounded w-4/5 mx-auto">
+        <input type="radio" name="my-accordion-3" checked="checked" />
+        <div className="collapse-title text-2xl rehn-bold text-accent">
+          Compare SQL and NoSQL databases?
+        </div>
+        <div className="collapse-content w-5/6 text-justify">
+          <p>
+            <span className="rehn-bold text-accent2">SQL databases:</span>{" "}
+            <br /> Follow a structured data model with predefined schemas. Use
+            tables with rows and columns to organize data. Ensure data
+            consistency and enforce relationships through constraints. Suitable
+            for complex and structured data with well-defined schemas. Support
+            powerful querying capabilities with SQL language.
+          </p>
+          <br />
+          <p>
+            <span className="rehn-bold text-accent2">NoSQL databases:</span>{" "}
+            <br />
+            Embrace a flexible and schema-less data model. Store data in various
+            formats like key-value pairs, documents, or graphs. Allow for quick
+            and easy scalability and horizontal expansion. Better suited for
+            unstructured or semi-structured data. Provide high performance and
+            flexible data access.
+          </p>
+        </div>
+      </div>
+
+      {/* third answer */}
+      <div className="collapse collapse-plus border-b border-accent rounded w-4/5 mx-auto">
+        <input type="radio" name="my-accordion-3" checked="checked" />
+        <div className="collapse-title text-2xl rehn-bold text-accent">
+          What is express js? What is Nest JS?
+        </div>
+        <div className="collapse-content w-5/6 text-justify">
+          <p>
+            <span className="rehn-bold text-accent2">Express.js</span> <br />{" "}
+            Express.js is a popular web application framework for Node.js. It
+            provides a minimalistic and flexible approach to building web
+            applications and APIs. Express.js simplifies tasks such as routing,
+            handling HTTP requests and responses, middleware integration, and
+            managing server-side logic.
+          </p>
+          <br />
+          <p>
+            <span className="rehn-bold text-accent2">Nest.js</span> <br />
+            Nest.js is a progressive, efficient, and extensible Node.js
+            framework for building scalable and maintainable server-side
+            applications. It is built on top of Express.js and adds additional
+            features and architectural patterns, such as dependency injection,
+            modular architecture, and decorators, which promote a structured and
+            organized codebase.
+          </p>
+        </div>
+      </div>
+
+      {/* fourth answer */}
+      <div className="collapse collapse-plus border-b border-accent rounded w-4/5 mx-auto">
+        <input type="radio" name="my-accordion-3" checked="checked" />
+        <div className="collapse-title text-2xl rehn-bold text-accent">
+          What is MongoDB aggregate and how does it work?
+        </div>
+        <div className="collapse-content w-5/6 text-justify">
+          <p>
+            <span className="rehn-bold text-accent2">MongoDBs aggregate</span>{" "}
+            <br />It is a feature that allows you to perform advanced data
+            analysis and manipulation on collections of documents. It works by
+            applying a sequence of operations, such as filtering, grouping,
+            sorting, and calculating aggregate values, to the documents in a
+            collection. This enables you to extract valuable insights and
+            transform your data in a flexible and efficient manner.
+          </p>
+          <br />
+        </div>
+      </div>
     </div>
   );
 };
